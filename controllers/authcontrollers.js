c87983db5fa5db1de8bbbd839db167f52e887dc5
Shiv <123@gmail.com>
2026-02-23 12:55:58 +0530

    Protected acces made and tested

--- controllers/authcontrollers.js
+++ controllers/authcontrollers.js
@@ -2,9 +2,9 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
+const generateToken = (id, role) => {
+  return jwt.sign({ id, role }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
   });
 };
 
--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,9 +1,19 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authcontrollers.js";
+import protect from "../middlewares/authmiddlewares.js";
+import authorizeRoles from "../middlewares/authorize.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+router.get(
+  "/profile",
+  protect,
+  (req, res) => {
+    res.json(req.user);
+  }
+);
 
 export default router;
